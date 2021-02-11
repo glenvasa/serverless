d96aba961fbd73134f960f0f17adf31378d15ba6
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-11 10:09:31 -0500

    Update 1-hello.js

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,9 +1,15 @@
 // domain/.netlify/functions/1-hello
 
-exports.handler = async (event, context) => {
+// const person = { name: "john" };
+
+// fn name handler is async b/c returns a promise
+// netlify provides the event and context
+exports.handler = async (event, context, cb) => {
   console.log(context);
   return {
     statusCode: 200,
-    body: "Our first Netlify function example",
+    body: "Our first Netlify Function Example",
+    // fn response must be a string; if an object, must be stringified
+    // body: JSON.stringify(person),
   };
 };
