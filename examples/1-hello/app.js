23d84148fa19d56619c13f4a85c6fcdce840219a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-12 17:35:59 -0500

    Update app.js

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -1,8 +1,11 @@
+const { default: axios } = require("axios");
+
 const result = document.querySelector(".result");
 
 const fetchData = async () => {
   try {
-    const { data } = await axios.get("/.netlify/functions/1-hello");
+    // const { data} = await axios.get('./netlify/functions/1-hello')
+    const { data } = await axios.get("/api/1-hello");
     result.textContent = data;
   } catch (error) {
     console.log(error.response.data);
